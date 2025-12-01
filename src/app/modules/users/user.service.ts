f57c0ba92550ace4e019c50564f3be76e29c5499
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-12-01 12:42:56 +0600

    fixed schema

--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -1,8 +1,13 @@
 /** biome-ignore-all lint/style/useImportType: > */
+import { prisma } from "../../configs/db.config";
 import { ICreateExplorer } from "./user.interface";
 
 const createExplorer = async (payload:ICreateExplorer) => {
-  console.log(payload);
+ // travelStylesTags e.g., ["Mountain", "Campigning",]
+ // intrests e.g., ["Backpacker", "Luxury", "Budget"]
+    // const newexplorer = await prisma.$transaction(async(trans)=>{
+    //     const newUser = await trans.
+    // })
 };
 
 export const userService = {
