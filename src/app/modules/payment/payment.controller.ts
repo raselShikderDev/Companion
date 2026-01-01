de9d9645a206dad1f00c2fba41c81762cc1d25f1
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-01-01 19:17:40 +0600

    added success and fail url

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -6,22 +6,59 @@ import sendResponse from "../../shared/sendResponse";
 import { StatusCodes } from "http-status-codes";
 import { PaymentService } from "./payment.service";
 
+const markPaymentFailed = catchAsync(async (req: Request, res: Response) => {
+  const tranId = req.body.tran_id || req.query.tran_id;
+
+  await PaymentService.markPaymentFailed(tranId, req.body);
+});
+
+/**
+ * CANCEL callback
+ */
+const markPaymentCancelled = catchAsync(async (req: Request, res: Response) => {
+  const tranId = req.body.tran_id || req.query.tran_id;
+
+  await PaymentService.markPaymentCancelled(tranId, req.body);
+});
+
 const getAllPayment = catchAsync(async (req: Request, res: Response) => {
-    const data = await PaymentService.getAllPayment(req.query as Record<string, string>);
-    sendResponse(res, { success: true, statusCode: StatusCodes.OK, message: "All payments", data:data.data,
-        meta:data.meta
-     });
+  const data = await PaymentService.getAllPayment(
+    req.query as Record<string, string>
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "All payments",
+    data: data.data,
+    meta: data.meta,
+  });
 });
 
 const getSinglePayment = catchAsync(async (req: Request, res: Response) => {
-    const data = await PaymentService.getSinglePayment(req.params.id);
-    sendResponse(res, { success: true, statusCode: StatusCodes.OK, message: "Payment fetched", data });
+  const data = await PaymentService.getSinglePayment(req.params.id);
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "Payment fetched",
+    data,
+  });
 });
 
 const getMyPayments = catchAsync(async (req: Request, res: Response) => {
-    const userId = req.user?.id;
-    const data = await PaymentService.getMyPayments(userId as string);
-    sendResponse(res, { success: true, statusCode: StatusCodes.OK, message: "Your payments", data });
+  const userId = req.user?.id;
+  const data = await PaymentService.getMyPayments(userId as string);
+  sendResponse(res, {
+    success: true,
+    statusCode: StatusCodes.OK,
+    message: "Your payments",
+    data,
+  });
 });
 
-export const PaymentController = { getAllPayment, getSinglePayment, getMyPayments };
+export const PaymentController = {
+  getAllPayment,
+  getSinglePayment,
+  getMyPayments,
+  markPaymentFailed,
+  markPaymentCancelled,
+};
--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -9,5 +9,7 @@ const router = Router();
 router.get("/all", checkAuth(Role.ADMIN), PaymentController.getAllPayment);
 router.get("/single/:id", checkAuth(Role.ADMIN), PaymentController.getSinglePayment);
 router.get("/my-pyment", checkAuth(Role.EXPLORER, Role.ADMIN), PaymentController.getMyPayments);
+router.get("/fail", checkAuth(Role.EXPLORER, Role.ADMIN), PaymentController.markPaymentFailed);
+router.get("/cancel", checkAuth(Role.EXPLORER, Role.ADMIN), PaymentController.markPaymentCancelled);
 
 export const paymentRouter = router;
