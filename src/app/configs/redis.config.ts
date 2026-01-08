da9a3b6950f473358d42156f7980242960c2e52d
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2026-01-09 00:58:04 +0600

    redis config

--- src/app/configs/redis.config.ts
+++ src/app/configs/redis.config.ts
@@ -1,7 +1,40 @@
-/* eslint-disable no-console */
+/** biome-ignore-all lint/style/useImportType: > */
 /** biome-ignore-all lint/suspicious/noExplicitAny: > */
-import { createClient } from 'redis';
-import { envVars } from './envVars';
+import { createClient, RedisClientType } from "redis";
+
+import { envVars } from "./envVars";
+
+let redis: RedisClientType | null = null;
+
+export async function getRedis(): Promise<RedisClientType> {
+  if (!redis) {
+    redis = createClient({
+      username: envVars.REDIS.REDIS_USERNAME,
+      password: envVars.REDIS.REDIS_PASSWORD,
+      socket: {
+        host: envVars.REDIS.REDIS_HOST,
+        port: Number(envVars.REDIS.REDIS_PORT),
+      },
+    });
+
+    redis.on("error", (err) => {
+      console.error("Redis Client Error", err);
+      redis = null; // force reconnect next time
+    });
+  }
+
+  if (!redis.isOpen) {
+    await redis.connect();
+  }
+
+  return redis;
+}
+
+
+// /* eslint-disable no-console */
+// /** biome-ignore-all lint/suspicious/noExplicitAny: > */
+// import { createClient } from 'redis';
+// import { envVars } from './envVars';
 
 export const redisClient = createClient({
     username: envVars.REDIS.REDIS_USERNAME,
@@ -15,9 +48,9 @@ export const redisClient = createClient({
 redisClient.on('error', (err: any) => console.log('Redis Client Error', err));
 
 
-// await redisClient.set('foo', 'bar');
-// const result = await redisClient.get('foo');
-// console.log(result)  // >>> bar
+// // await redisClient.set('foo', 'bar');
+// // const result = await redisClient.get('foo');
+// // console.log(result)  // >>> bar
 
 
 export const connectRedis = async()=>{
@@ -26,3 +59,12 @@ export const connectRedis = async()=>{
         console.log("Redis Successfully connected");
     }
 }
+
+// // redis.ts
+// export const getRedis = async () => {
+//   if (!redisClient.isOpen) {
+//     await redisClient.connect()
+//     console.log("Redis connected")
+//   }
+//   return redisClient
+// }
